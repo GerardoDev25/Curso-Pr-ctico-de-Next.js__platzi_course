74a9376dbc384c6532c1479eec41db41d80c9a96
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-04 10:24:58 -0400

    React Context en el Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Menu from '@components/Menu';
 import MyOrder from '../containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
-import AppContext from '../context/AppContext';
+import AppContext from '@context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 import style from '@styles/Header.module.scss';
