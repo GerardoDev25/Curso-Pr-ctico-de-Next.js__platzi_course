40e98e9054f53b0845791deaca5e65708ca63cc6
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-08 09:47:32 -0400

    Curso Práctico de Next.js

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import Head from 'next/head';
 import OrderItem from '@components/OrderItem';
 import style from '@styles/Checkout.module.scss';
 
 const Checkout = () => {
   return (
-    <div className={style.Checkout}>
-      <div className={style['Checkout-container']}>
-        <h1 className={style.title}>My order</h1>
-        <div className={style['Checkout-content']}>
-          <div className={style.order}>
-            <p>
-              <span>03.25.21</span>
-              <span>6 articles</span>
-            </p>
-            <p>$560.00</p>
+    <>
+      <Head>
+        <title>Checout</title>
+      </Head>
+      <div className={style.Checkout}>
+        <div className={style['Checkout-container']}>
+          <h1 className={style.title}>My order</h1>
+          <div className={style['Checkout-content']}>
+            <div className={style.order}>
+              <p>
+                <span>03.25.21</span>
+                <span>6 articles</span>
+              </p>
+              <p>$560.00</p>
+            </div>
           </div>
+          {/* <OrderItem /> */}
         </div>
-        <OrderItem />
       </div>
-    </div>
+    </>
   );
 };
 
